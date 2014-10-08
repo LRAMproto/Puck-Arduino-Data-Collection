864fc0127d52c6ba914f13ea28f2b47e6ff5f9c7
caster <caster@LRAM-dev-1.(none)>
2014-10-07 17:05:10 -0700

    Organized data structs into a single super-struct for easy export.

--- Arduino_Data_Collector_2.m
+++ Arduino_Data_Collector_2.m
@@ -2,31 +2,31 @@
 %
 % Refactoring by David Rebhuhn
 
-function Arduino_Data_Collector_2()
+function h = Arduino_Data_Collector_2()
 
-% Captures settings between calls.
-settings = struct();
+% Captures h.handles between calls.
+h.settings = struct();
 
 % Captures runtime variables during calls.
-runtime_vars = struct();
+h.runtime_vars = struct();
 
-% Captures data during calls.
-data = struct();
+% Captures h.data during calls.
+h.data = struct();
 
 if ispc
     % Checks if the Windows version of MATLAB is being used.
     % Sets port name accordingly.
-    settings.PORT_NAME = 'COM1';
+    h.settings.PORT_NAME = 'COM1';
     
 elseif ismac
     % Checks if the Macintosh version of MATLAB is being used.
     % Sets port name accordingly
-    settings.PORT_NAME ='/dev/ttyACM0';
+    h.settings.PORT_NAME ='/dev/ttyACM0';
     
 elseif isunix
     % Checks if the UNIX version of MATLAB is being used.
     % Sets port name accordingly.
-    settings.PORT_NAME ='/dev/ttyACM0';
+    h.settings.PORT_NAME ='/dev/ttyACM0';
     
 else
     
@@ -37,103 +37,103 @@ end
 % Locate Arduino
 %
 
-runtime_vars.arduino = serial(settings.PORT_NAME,'BaudRate',9600);
+h.runtime_vars.arduino = serial(h.settings.PORT_NAME,'BaudRate',9600);
 
 % Begin Communication with Arduino
-fopen(runtime_vars.arduino);
+fopen(h.runtime_vars.arduino);
 
 try
     
-    % Specify number of runs to collect data from.
-    settings.NUM_RUNS = 1001;
+    % Specify number of runs to collect h.data from.
+    h.settings.NUM_RUNS = 1001;
     
     % Initiate radio reading with a 1 second pause to filter out cutoff terms
     % pause(1)
-    data1 = fscanf(runtime_vars.arduino);
+    h.data1 = fscanf(h.runtime_vars.arduino);
     pause(1)
     
-    for i = 1:settings.NUM_RUNS
-        input_data = fscanf(runtime_vars.arduino,'%f');
+    for i = 1:h.settings.NUM_RUNS
+        input_h.data = fscanf(h.runtime_vars.arduino,'%f');
         
-        % compile data into a vector
-        data.raw(i) = input_data(1);
+        % compile h.data into a vector
+        h.data.raw(i) = input_h.data(1);
         
     end
     
-    fclose(runtime_vars.arduino)
+    fclose(h.runtime_vars.arduino)
     
 catch err
     
-    % This allows the runtime_vars.arduino to be closed neatly in the case of some
+    % This allows the h.runtime_vars.arduino to be closed neatly in the case of some
     % strange error occurring.
     
-    fclose(runtime_vars.arduino);
+    fclose(h.runtime_vars.arduino);
     rethrow(err);
 end
 
-% Cutoff first 10 terms of Xdata.raw due to communication delays between
+% Cutoff first 10 terms of Xh.data.raw due to communication delays between
 % radio read
-data.cut = data.raw(10:length(data.raw));
+h.data.cut = h.data.raw(10:length(h.data.raw));
 
-% View compiled acceleration data
-data.accel_vector = reshape(data.cut,2,[]);
+% View compiled acceleration h.data
+h.data.accel_vector = reshape(h.data.cut,2,[]);
 
 
 % %
 % % *********** Calibration and Conversion **************
 % %
-% % //// Convert [mV] data into G-forces and calibrate each axis ////
+% % //// Convert [mV] h.data into G-forces and calibrate each axis ////
 % %
 
 % %
-% % X data
+% % X h.data
 % %
-data.x_data = data.accel_vector(1,:);
-data.xavg = mean(data.x_data);
+h.data.x_h.data = h.data.accel_vector(1,:);
+h.data.xavg = mean(h.data.x_h.data);
 %                                      % Arduino Nano_usb = 334.1696 [mV/g]
 %                                      % Arduino Nano_bat = 403.8696 [mV/g]
 %                                      % Arduino Fio_usb = 502.2153 [mV/g]
 %                                      % Arduino Fio_bat = 503.0357 [mV/g]
-data.g_force_x = (data.x_data ./ 503.0357)-1;
-data.max_g_x = max(data.g_force_x);
+h.data.g_force_x = (h.data.x_h.data ./ 503.0357)-1;
+h.data.max_g_x = max(h.data.g_force_x);
 
 % %
-% % Y data
+% % Y h.data
 % %
-data.y_data = data.accel_vector(2,:);
-data.yavg = mean(data.y_data);
+h.data.y_h.data = h.data.accel_vector(2,:);
+h.data.yavg = mean(h.data.y_h.data);
 %                                      % Arduino Nano_usb = 338.7065 [mV/g]
 %                                      % Arduino Nano_bat = 406.4761 [mV/g]
 %                                      % Arduino Fio_usb = 514.9405 [mV/g]
 %                                      % Arduino Fio_bat = 516.0575 [mV/g]
-data.g_force_y = (data.y_data ./ 516.0575)-1;
-data.max_g_y = max(data.g_force_y);
+h.data.g_force_y = (h.data.y_h.data ./ 516.0575)-1;
+h.data.max_g_y = max(h.data.g_force_y);
 
 % %
-% % Z data
+% % Z h.data
 % %
-% z_data = data.accel_vector(3,:)
-% z_data = data.cut;
-% zavg = mean(z_data);
+% z_h.data = h.data.accel_vector(3,:)
+% z_h.data = h.data.cut;
+% zavg = mean(z_h.data);
 % %                                     % Arduino Nano_usb = 403.6892 [mV/g]
 % %                                     % Arduino Nano_bat = 484.2441 [mV/g]
 % %                                     % Arduino Fio_usb =
 % %                                     % Arduino Fio_bat =
-% g_force_z = abs(z_data ./ 405.0106);
+% g_force_z = abs(z_h.data ./ 405.0106);
 % max_g_z = max(g_force_z);
 
 
 
-% calculate the data.resultant
-data.resultant = sqrt(data.g_force_x.^2 + data.g_force_y.^2);
-data.maxR = max(abs(data.resultant));
+% calculate the h.data.resultant
+h.data.resultant = sqrt(h.data.g_force_x.^2 + h.data.g_force_y.^2);
+h.data.maxR = max(abs(h.data.resultant));
 
 % %
 % % Plot results
 % % ********* Data **********
 % %
-plot([0:length(data.accel_vector)-1],data.resultant,'linewidth',1.1)
-title('Acceleration data.resultant vs Time')
+plot([0:length(h.data.accel_vector)-1],h.data.resultant,'linewidth',1.1)
+title('Acceleration h.data.resultant vs Time')
 ylabel('Acceleration [G force]')
 xlabel('Time [unknown units]')
 grid on
@@ -142,11 +142,11 @@ grid on
 
 
 % %
-% % Terminate the terminal and clear "runtime_vars.arduino"
+% % Terminate the terminal and clear "h.runtime_vars.arduino"
 % %
 
-delete(runtime_vars.arduino)
-clear runtime_vars.arduino
+delete(h.runtime_vars.arduino)
+clear h.runtime_vars.arduino
 
 
 end
